077514bd146c4d01712bdf4d3d5d5556119960e3
rulingfeng <512387589@qq.com>
2022-11-15 09:56:12 +0800

    11

--- order/src/main/java/com/rlf/controller/OrderController.java
+++ order/src/main/java/com/rlf/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.rlf.controller;
 
+import com.google.common.collect.Lists;
 import com.rlf.config.Constants;
 import com.rlf.feign.GoodsApi;
 import com.rlf.feign.StoreApi;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  * @author rulingfeng
@@ -37,10 +40,14 @@ public class OrderController {
 
     @GetMapping("/bbb")
     public Result<String> bbb(){
-        OrderMain orderMain = new OrderMain();
-        orderMain.setOrderId(888l);
-        orderMain.setOrderNo(888l);
-        orderMainService.save(orderMain);
+        ArrayList<OrderMain> objects = Lists.newArrayList();
+        for (long i = 0; i < 4 ; i++) {
+            OrderMain orderMain = new OrderMain();
+            orderMain.setOrderId(888l);
+            orderMain.setOrderNo(i);
+            objects.add(orderMain);
+        }
+        orderMainService.saveBatch(objects);
         Result<String> bbbb = goodsApi.bbbb(777l);
         System.out.println(bbbb);
         return storeApi.bbbb(666l);
