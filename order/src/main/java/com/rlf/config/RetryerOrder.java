486180f9152027d83700107c7cb47fdc254cfa2c
rulingfeng <rulf@inm.cc>
2022-11-28 10:11:59 +0800

    解决第一次启动， feign重复执行

---
+++ order/src/main/java/com/rlf/config/RetryerOrder.java
@@ -0,0 +1,75 @@
+package com.rlf.config;
+
+import feign.RetryableException;
+import feign.Retryer;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author rulingfeng
+ * @time 2022/11/28 09:29
+ * @desc
+ */
+public class RetryerOrder implements Retryer {
+    private final int maxAttempts;
+    private final long period;
+    private final long maxPeriod;
+    int attempt;
+    long sleptForMillis;
+
+    public RetryerOrder() {
+        this(100L, TimeUnit.SECONDS.toMillis(1L), 1);
+    }
+
+    public RetryerOrder(long period, long maxPeriod, int maxAttempts) {
+        this.period = period;
+        this.maxPeriod = maxPeriod;
+        this.maxAttempts = maxAttempts;
+        this.attempt = 1;
+    }
+
+    protected long currentTimeMillis() {
+        return System.currentTimeMillis();
+    }
+    @Override
+    public void continueOrPropagate(RetryableException e) {
+        if (this.attempt++ >= this.maxAttempts) {
+            throw e;
+        } else {
+            long interval;
+            if (e.retryAfter() != null) {
+                interval = e.retryAfter().getTime() - this.currentTimeMillis();
+                if (interval > this.maxPeriod) {
+                    interval = this.maxPeriod;
+                }
+
+                if (interval < 0L) {
+                    return;
+                }
+            } else {
+                interval = this.nextMaxInterval();
+            }
+
+            try {
+                Thread.sleep(interval);
+            } catch (InterruptedException var5) {
+                Thread.currentThread().interrupt();
+                throw e;
+            }
+
+            this.sleptForMillis += interval;
+        }
+    }
+
+
+    long nextMaxInterval() {
+        long interval = (long)((double)this.period * Math.pow(1.5, (double)(this.attempt - 1)));
+        return interval > this.maxPeriod ? this.maxPeriod : interval;
+    }
+
+    @Override
+    public Retryer clone() {
+        return new Default(this.period, this.maxPeriod, this.maxAttempts);
+    }
+}
+
